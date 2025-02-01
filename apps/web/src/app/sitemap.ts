fea845e66e48be50a6633f02173550b7020d430a
nriin-trizeki <rizeki.rifandani@nri.co.id>
2025-02-01 23:16:11 +0700

    feat(web): sitemap

---
+++ apps/web/src/app/sitemap.ts
@@ -0,0 +1,35 @@
+import type { MetadataRoute } from 'next'
+import fs from 'node:fs'
+
+const appFolders = fs.readdirSync('app', { withFileTypes: true })
+const pages = appFolders
+  .filter(file => file.isDirectory())
+  .filter(folder => !folder.name.startsWith('_'))
+  .filter(folder => !folder.name.startsWith('('))
+  .map(folder => folder.name)
+// TODO: change production url when you know
+const url = new URL(
+  process.env.NODE_ENV === 'production' ? 'https://rifandani.com' : 'http://localhost:3002',
+)
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: new URL('/', url).href,
+      lastModified: new Date(),
+      // priority: 1,
+      // images: [new URL('/image.jpg', url).href],
+      // videos: [
+      //   {
+      //     title: 'example',
+      //     thumbnail_loc: new URL('/image.jpg', url).href,
+      //     description: 'this is the description',
+      //   },
+      // ],
+    },
+    ...pages.map(page => ({
+      url: new URL(page, url).href,
+      lastModified: new Date(),
+    })),
+  ]
+}
