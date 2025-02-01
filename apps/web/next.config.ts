d383a6c13d5a90c40383166cf8d8dc5aab80d07e
nriin-trizeki <rizeki.rifandani@nri.co.id>
2025-02-02 00:06:13 +0700

    feat(web): bundle analyzer

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,18 +1,31 @@
 import type { NextConfig } from 'next'
+import withBundleAnalyzer from '@next/bundle-analyzer'
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin('./src/core/i18n/request.ts')
 
-const nextConfig: NextConfig = {
+// eslint-disable-next-line import/no-mutable-exports
+let config: NextConfig = withNextIntl({
   output: 'standalone', // for deploying
   transpilePackages: [
     '@workspace/core',
     '@t3-oss/env-nextjs',
     '@t3-oss/env-core',
   ],
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
   experimental: {
     // testProxy: true, // need to enable for e2e testing
   },
+})
+
+function withAnalyzer(sourceConfig: NextConfig): NextConfig {
+  return withBundleAnalyzer()(sourceConfig)
 }
 
-export default withNextIntl(nextConfig)
+if (process.env.ANALYZE === 'true') {
+  config = withAnalyzer(withNextIntl(config))
+}
+
+export default config
