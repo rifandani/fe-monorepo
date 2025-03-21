b2f64b9dd9d7881f6fc8fae32f6079cdd602d2a8
rifandani <tri.rifandani@gmail.com>
2025-03-21 16:03:54 +0700

    fix(web): build errors

--- apps/web/src/core/constants/env.ts
+++ apps/web/src/core/constants/env.ts
@@ -6,10 +6,12 @@ export const ENV = createEnv({
   client: {
     NEXT_PUBLIC_TITLE: z.string().min(1),
     NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+    NEXT_PUBLIC_SITE_URL: z.string().url(),
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {
     NEXT_PUBLIC_TITLE: process.env.NEXT_PUBLIC_TITLE,
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
   },
 })
