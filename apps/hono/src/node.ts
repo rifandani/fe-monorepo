ab53ea0c029a3cc0e1fc24ac1f9556c643b12fbc
rifandani <tri.rifandani@gmail.com>
2025-04-15 13:18:24 +0700

    chore: remove hono app

--- apps/hono/src/node.ts
+++
@@ -1,8 +0,0 @@
-import { PORT } from '@/app/constants/global'
-import { serve } from '@hono/node-server'
-import { logger } from '@workspace/core/utils/logger'
-import { app } from './app'
-
-serve({ ...app, port: PORT }, (info) => {
-  logger.info(`Listening on http://localhost:${info.port}`)
-})
