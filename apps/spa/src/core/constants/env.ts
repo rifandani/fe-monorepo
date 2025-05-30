73d0c52921c985a9823cde93191c7bdfb0042945
rifandani <tri.rifandani@gmail.com>
2025-05-30 17:54:14 +0700

    fix: env example file

--- apps/spa/src/core/constants/env.ts
+++ apps/spa/src/core/constants/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 export const envSchema = z.object({
   VITE_APP_TITLE: z.string(),
   VITE_API_BASE_URL: z.string().url(),
+  VITE_SITE_URL: z.string().url(),
 })
 
 export const env = (() => {
@@ -12,9 +13,13 @@ export const env = (() => {
   const apiBaseUrl = envSchema.shape.VITE_API_BASE_URL.parse(
     import.meta.env.VITE_API_BASE_URL,
   )
+  const siteUrl = envSchema.shape.VITE_SITE_URL.parse(
+    import.meta.env.VITE_SITE_URL,
+  )
 
   return {
     appTitle,
     apiBaseUrl,
+    siteUrl,
   }
 })()
